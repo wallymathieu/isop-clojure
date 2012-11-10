c33b94d3703dd91187d224617ddd52b9ab915035
Oskar Gewalli <gewalli@gmail.com>
2012-11-10 12:07:02 +0100

    updated readme

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject simple-clojure-project "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :description "A simple project to try out clojure"
+  :dependencies [[org.clojure/clojure "1.3.0"], [com.datomic/datomic-free "0.8.3599"]])
