b061f92ed248cd8cb9f506c285c6204b3cba3539
Oskar Gewalli <gewalli@gmail.com>
2012-11-10 11:53:42 +0100

    Trying out clojure

---
+++ src/simple_clojure_project/core.clj
@@ -0,0 +1,1 @@
+(ns simple-clojure-project.core)
---
+++ test/simple_clojure_project/test/core.clj
@@ -0,0 +1,6 @@
+(ns simple-clojure-project.test.core
+  (:use [simple-clojure-project.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
