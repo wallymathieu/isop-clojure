e5bd6ad9ea15d8ab312c269bd520e8cc942e5237
Oskar Gewalli <gewalli@gmail.com>
2012-11-18 12:05:10 +0100

    Map given previous

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject simple-clojure-project "1.0.0-SNAPSHOT"
-  :description "A simple project to try out clojure"
-  :dependencies [[org.clojure/clojure "1.3.0"], [com.datomic/datomic-free "0.8.3599"]])
+(defproject isop "1.0.0-SNAPSHOT"
+  :description "Implementation of isop in clojure"
+  :dependencies [[org.clojure/clojure "1.3.0"]])
