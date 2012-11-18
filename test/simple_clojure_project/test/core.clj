7387b421c7dd295af17c947343051791bb77defb
Oskar Gewalli <gewalli@gmail.com>
2012-11-18 12:05:31 +0100

    Removed old namespace

--- test/simple_clojure_project/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns simple-clojure-project.test.core
-  (:use [simple-clojure-project.core])
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
